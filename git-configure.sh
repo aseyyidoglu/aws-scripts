4d0b0dbcbe2a06518c208a3a8fbfa9b4c4bd3afc
Ali Seyyidoglu <74444230+aseyyidoglu@users.noreply.github.com>
2023-03-04 12:04:02 +1100

    Update git-configure.sh

--- git-configure.sh
+++ git-configure.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+# !/bin/sh
 
 # Create SSH key
 ssh-keygen -t ed25519 -C "YOUR_EMAIL"
@@ -18,16 +18,3 @@ EOF
 # Test GitHub SSH Access
 echo Attempts to ssh to GitHub
 ssh -T git@github.com
-
-# Connect to remote respository and fetch
-git remote add origin git@github.com:USERNAME/REPOSITORY.git
-git fetch origin master
-echo yes
-
-# Overwrite your local files
-git fetch --all
-git reset --hard origin/master
-
-# Check current branches
-git branch -r # for remote
-git branch  # for local
