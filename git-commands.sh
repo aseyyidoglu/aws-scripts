981cca83987a468d7010d59299ed630750ad317d
Ali Seyyidoglu <74444230+aseyyidoglu@users.noreply.github.com>
2023-03-09 06:23:50 +1100

    Update git-commands.sh
    added more commands
    added Atlassian git reference article

--- git-commands.sh
+++ git-commands.sh
@@ -1,7 +1,20 @@
+# Reference: https://confluence.atlassian.com/bitbucketserver/basic-git-commands-776639767.html
+
+# Configuration
+git config --global user.name "Sam Smith"
+git config --global user.email sam@example.com
+
+# Make a copy of remote
+git clone /path/to/repository
+git clone username@host:/path/to/repository
+
 # Connect to remote respository and fetch
 git remote add origin git@github.com:USERNAME/REPOSITORY.git
-git fetch origin master
-echo yes
+git fetch origin REMOTE_BRANCH
+git merge origin/main
+
+# List remote repositories
+git remote -v
 
 # Overwrite your local files
 git fetch --all
