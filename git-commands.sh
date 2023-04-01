5727a0dec5221a77f27257b8695d55059de33a0a
Ali Seyyidoglu <74444230+aseyyidoglu@users.noreply.github.com>
2023-04-01 21:49:09 +1100

    Update git-commands.sh
    Added new commands

--- git-commands.sh
+++ git-commands.sh
@@ -1,9 +1,14 @@
 # Reference: https://confluence.atlassian.com/bitbucketserver/basic-git-commands-776639767.html
 
-# Configuration
+# GET STARTED
+# Configure git
 git config --global user.name "Sam Smith"
 git config --global user.email sam@example.com
 
+# Check existing configuration
+git config --list
+
+# WORKING WITH REMOTE DIRECTORY
 # Make a copy of remote
 git clone /path/to/repository
 git clone username@host:/path/to/repository
@@ -11,15 +16,15 @@ git clone username@host:/path/to/repository
 # Connect to remote respository and fetch
 git remote add origin git@github.com:USERNAME/REPOSITORY.git
 git fetch origin REMOTE_BRANCH
-git merge origin/main
+git merge origin/main #this could be: origin/master
 
 # List remote repositories
 git remote -v
 
-# Overwrite your local files
-git fetch --all
-git reset --hard origin/master
-
 # Check current branches
 git branch -r # for remote
 git branch  # for local
+
+# Overwrite your local files
+git fetch --all
+git reset --hard origin/master
