af57787ac656a6e87021b61ca42a79919dfb9d5f
Ali Osman Seyyidoglu <74444230+aseyyidoglu@users.noreply.github.com>
2024-04-23 17:29:54 +1000

    Update git-setup.sh
    Added github ssh key url in line 9

--- git-setup.sh
+++ git-setup.sh
@@ -6,6 +6,7 @@ ssh-keygen -t rsa -C ali.seyyidoglu@gmail.com
 # then press ENTER, add passphrase
 
 # Evaluate and add ssh key into id_rsa file, and open the public key file to copy for remote repo
+# Github SSH keys: https://github.com/settings/keys
 eval "$(ssh-agent -s)" && ssh-add ~/.ssh/id_rsa && open ~/.ssh/id_rsa.pub
 
 # Test connection over port 443
